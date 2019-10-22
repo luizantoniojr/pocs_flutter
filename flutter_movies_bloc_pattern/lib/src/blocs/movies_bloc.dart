8a9d915a28610af32bb4b06992a648f79995c221
luizantoniojr <luizantoniosilvajr@outlook.com>
2019-10-21 21:14:37 -0300

    .

--- flutter_movies_bloc_pattern/lib/src/blocs/movies_bloc.dart
+++ flutter_movies_bloc_pattern/lib/src/blocs/movies_bloc.dart
@@ -1,11 +1,19 @@
-import 'package:rxdart/rxdart.dart';
-
 import '../resources/repository.dart';
+import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
+import 'package:inject/inject.dart';
+import 'bloc_base.dart';
 
-class MoviesBloc {
-  final _repository = Repository();
-  final _moviesFetcher = PublishSubject<ItemModel>();
+class MoviesBloc extends BlocBase {
+  final Repository _repository;
+  PublishSubject<ItemModel> _moviesFetcher;
+
+  @provide
+  MoviesBloc(this._repository);
+
+  init() {
+    _moviesFetcher = PublishSubject<ItemModel>();
+  }
 
   Observable<ItemModel> get allMovies => _moviesFetcher.stream;
 
@@ -14,9 +22,8 @@ class MoviesBloc {
     _moviesFetcher.sink.add(itemModel);
   }
 
+  @override
   dispose() {
     _moviesFetcher.close();
   }
 }
-
-final bloc = MoviesBloc();
--- flutter_movies_bloc_pattern/lib/src/resources/movie_api_provider.dart
+++ flutter_movies_bloc_pattern/lib/src/resources/movie_api_provider.dart
@@ -1,19 +1,30 @@
 import 'dart:async';
-import 'package:flutter_movies_bloc_pattern/src/models/trailer_model.dart';
-import 'package:http/http.dart' show Client;
 import 'dart:convert';
+import 'package:http/http.dart' show Client, Response;
 import '../models/item_model.dart';
+import '../models/trailer_model.dart';
+import 'package:inject/inject.dart';
 
 class MovieApiProvider {
-  Client client = Client();
-  final _apiKey = '024a472aaeb432aa1fda5faabafeb48d';
+  final Client client;
+  final _apiKey = 'api-key';
   final _baseUrl = "http://api.themoviedb.org/3/movie";
 
+  @provide
+  MovieApiProvider(this.client);
+
   Future<ItemModel> fetchMovieList() async {
-    final response = await client.get("$_baseUrl/popular?api_key=$_apiKey");
+    Response response;
+    if (_apiKey != 'api-key') {
+      response = await client.get("$_baseUrl/popular?api_key=$_apiKey");
+    } else {
+      throw Exception('Please add your API key');
+    }
     if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
       return ItemModel.fromJson(json.decode(response.body));
     } else {
+      // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
     }
   }
